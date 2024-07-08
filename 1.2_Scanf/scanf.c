f9d13e234acfb6488ebccb07a405b73b23ac0933
HuseyinErkek <115713946+HuseyinErkek@users.noreply.github.com>
2024-07-08 11:00:20 +0300

    Kod icergi duzenlendi

--- 1.2_Scanf/scanf.c
+++ 1.2_Scanf/scanf.c
@@ -6,22 +6,27 @@ int main()
     // Integer
     int sayi;
     printf("Lutfen integer sayi giriniz :\n");
-    scanf("%d",&sayi);
-    printf("Girdigimiz interger sayi : %d\n",sayi);
+    scanf("%d", &sayi);
+    printf("Girdiginiz integer sayi : %d\n", sayi);
+    
     // Float 
-   float sayi1;
+    float sayi1;
     printf("Lutfen float turunde bir sayi giriniz :\n");
-    scanf("%f",&sayi1);
-    printf("Girdigimiz float sayi : %.2f\n",sayi1);
-    //Double
+    scanf("%f", &sayi1);
+    printf("Girdiginiz float sayi : %.2f\n", sayi1);
+    
+    // Double
     double sayi2;
     printf("Lutfen double bir sayi giriniz :\n");
-    scanf("%lf",&sayi2);
-    printf("Girdiginiz double sayi : %.1lf\n",sayi2);}
-   
-    //Char
+    scanf("%lf", &sayi2);
+    printf("Girdiginiz double sayi : %.1lf\n", sayi2);
+    
+    // Char
     char harf;
-    printf("Luften char deger giriniz :\n");
-    scanf("%c",&harf);
-    printf("Girdiginiz char deger :%c\n",harf);
+    printf("Lutfen char deger giriniz :\n");
+    // scanf'den önce bir boşluğu okuması için bir boşluk eklemeliyiz.
+    scanf(" %c", &harf);
+    printf("Girdiginiz char deger : %c\n", harf);
+
+    return 0;
 }
